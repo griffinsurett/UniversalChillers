2f82fa8b2e63ca439b003bce03cb36d81be8cf0e
griffinjoshs <leveloutusa@gmail.com>
2025-03-17 00:06:27 -0400

    modals

--- src/components/Menu/SubMenu.jsx
+++ src/components/Menu/SubMenu.jsx
@@ -1,16 +1,16 @@
-// src/components/Menu/SubMenu.jsx
+// This React component renders the submenu as a list of MenuItems (it can reuse your Astro version or a React version).
 import React from "react";
-import MenuItem from "./MenuItem.jsx";
+import MenuItem from "./MenuItem.jsx"; // You can reuse the React version if desired
 
 export default function SubMenu({ items, itemClass, listClass, maxDepth, currentDepth }) {
   return (
     <ul className={listClass}>
-      {items.map((item) => (
-        <MenuItem 
-          key={item.id} 
-          item={item} 
-          itemClass={itemClass} 
-          listClass={listClass} 
+      {items.map((child) => (
+        <MenuItem
+          key={child.id}
+          item={child}
+          itemClass={itemClass}
+          listClass={listClass}
           maxDepth={maxDepth}
           currentDepth={currentDepth}
         />
