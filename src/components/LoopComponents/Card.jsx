9382165e88e9d4ad82e38bc195164bda560abb9e
griffinjoshs <leveloutusa@gmail.com>
2025-03-19 14:20:57 -0400

    card div to li for accessibility

--- src/components/LoopComponents/Card.jsx
+++ src/components/LoopComponents/Card.jsx
@@ -3,7 +3,7 @@ import Button from "../Button.jsx";
 // Example: Card.jsx (React)
 export default function Card({ item, collectionName }) {
   return (
-    <li className="card py-4">
+    <li className="card">
       <h3>{item.data.title}</h3>
       <p>{item.data.description || item.body}</p>
       <Button href={`/${collectionName}/${item.slug}`}>View More</Button>
