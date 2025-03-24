5949ac24c6b5b564d9a6274d90dc628ef2828c79
griffinjoshs <leveloutusa@gmail.com>
2025-03-24 12:10:11 -0400

    haspage

--- src/components/LoopComponents/Card.jsx
+++ src/components/LoopComponents/Card.jsx
@@ -6,9 +6,9 @@ import Button from "../Button.jsx";
  * otherwise the default provided via Section meta) to decide
  * whether to render the "View More" button.
  */
-export default function Card({ item, collectionName, defaultHasPage }) {
+export default function Card({ item, collectionName, HasPage }) {
   const effectiveHasPage =
-    item.data.hasPage !== undefined ? item.data.hasPage : defaultHasPage;
+    item.data.hasPage !== undefined ? item.data.hasPage : HasPage;
   return (
     <li className="card">
       <h3>{item.data.title}</h3>
