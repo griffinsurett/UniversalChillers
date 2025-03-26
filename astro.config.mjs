7292167ae4067e3df242b07681c8e019ca1c9af6
griffinjoshs <leveloutusa@gmail.com>
2025-03-26 15:02:54 -0400

    vite config split

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
   server: {
-    port: 9000 // Change this to your desired port
+    port: 9000,
   },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            // Split node_modules into separate chunks for each package
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0];
+            }
+          },
+        },
+      },
+    },
   },
-
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react()],
 });
