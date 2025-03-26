53e47a259edcc9dc69308a935c93f0d47e6d25bf
griffinjoshs <leveloutusa@gmail.com>
2025-03-26 14:32:43 -0400

    tagline

--- src/content/SiteData.js
+++ src/content/SiteData.js
@@ -3,6 +3,7 @@ import Logo from "@/assets/astro.svg"
 
 export const SiteData = {
     title: "Griffins Site",
+    tagline: "Griffin's personal website for learning astro.js", 
     description: "This is a site for Griffin to sit down and learn astro.js and start his journey of going pro",
     logo: {
         src: Logo,
