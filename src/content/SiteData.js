aa5fed976136ada76440317eb46ea2f6a57d84dd
griffinjoshs <leveloutusa@gmail.com>
2025-07-30 03:36:46 -0400

    cb section

--- src/content/SiteData.js
+++ src/content/SiteData.js
@@ -4,7 +4,7 @@ import Logo from "@/assets/UniversalChillers.png"
 export const SiteData = {
     title: "Universal Chillers",
     tagline: "Universal Chillers delivers heating and cooling systems with the power, precision, and performance your facility needs.", 
-    description: "This is a site for Griffin to sit down and learn astro.js and start his journey of going pro",
+    description: "From rooftop chillers to boiler rooms, cooling towers, and industrial-grade boilers, our systems are engineered for serious performance. Whether it's a new install, urgent repair, or long-term maintenance, we keep America chillin.",
     logo: {
         src: Logo,
         alt: "Universal Chillers logo",
